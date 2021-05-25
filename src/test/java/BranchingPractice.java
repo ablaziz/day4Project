0c03670c0ab8c96600592442bbe4dfd0c78f1080
Aziz Niada <niadaaaziz@yahoo.fr>
2021-05-25 19:24:03 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //create a new project
+        //enabled version control
+        //add .gitignore wit .idea, target folder and iml file
+        //initial commit
+        //shared on github
 
     }
 }
